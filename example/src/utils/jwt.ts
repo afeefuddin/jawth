91e514a675a7bdc1fb685404bf819ae35359636e
Afeefuddin <afeefud2004din@gmail.com>
2024-07-29 22:31:49 +0530

    Updated the npm package name

--- example/src/utils/jwt.ts
+++ example/src/utils/jwt.ts
@@ -1,16 +1,16 @@
-import * as jawthh from "jawthh"
+import * as jawth from "jawth"
 
 export async function create_access_token(id: string,username:string){
     const secret = process.env.ACCESS_TOKEN_SECRET!
 
-    const token = await jawthh.encode_jwt(secret,id,{username})
+    const token = await jawth.encode_jwt(secret,id,{username})
     return token
 }
 
 
 export async function create_refresh_token(id: string,username: string){
     const secret = process.env.REFRESH_TOKEN_SECRET!
-    const token = await jawthh.encode_jwt(secret,id,{
+    const token = await jawth.encode_jwt(secret,id,{
         username
     })
     return token
@@ -19,7 +19,7 @@ export async function create_refresh_token(id: string,username: string){
 export async function verify_token(jwt: string){
     const secret = process.env.ACCESS_TOKEN_SECRET!
 
-    const isValid = await jawthh.decode_jwt(secret,jwt);
+    const isValid = await jawth.decode_jwt(secret,jwt);
     if(isValid){
         return true;
     }
@@ -29,6 +29,6 @@ export async function verify_token(jwt: string){
 export async function getIdfromjwt(jwt:string) {
     const secret = process.env.ACCESS_TOKEN_SECRET!
     
-    const data = await jawthh.decode_jwt(secret,jwt);
+    const data = await jawth.decode_jwt(secret,jwt);
     return data.id
 }
